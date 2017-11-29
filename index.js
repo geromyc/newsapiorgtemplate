9f7dda12837ce5b4f4558930aacc9d1cc9ca135b
Geromy Cunningham <geromycunningham@gmail.com>
2017-11-29 00:05:02 +0000

    addition challenges all complete

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ $(document).ready(function() {
     //     });
     $('#source').submit(function(event) {
         event.preventDefault();
+        document.getElementById("display").innerHTML = "";  
         $.ajax({
             method: "GET",
             url: "https://newsapi.org/v2/top-headlines",
@@ -30,9 +31,11 @@ $(document).ready(function() {
                 if (event.status === "ok") {
                     console.log(event)
                     for (var i = 0; i < event.articles.length; i++) {
-                        var source = document.createElement("LI");
-                        source.setAttribute("value", event.articles[i].title);
-                        source.innerHTML = event.articles[i].title;
+                        var source = document.createElement("A");
+                        source.setAttribute("class", "col-xs-4");
+                        source.setAttribute("style", "height: 350px");
+                        source.setAttribute("href", event.articles[i].url);
+                        source.innerHTML = "<img src="+event.articles[i].urlToImage+" height= 100vh> </br> "+event.articles[i].title+" </br> "+event.articles[i].description+"";
                         document.getElementById('display').appendChild(source);
                     }
                 }

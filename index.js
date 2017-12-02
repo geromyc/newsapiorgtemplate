fdc2f148e6b2670f86da8db2fcc5626b052687b4
Geromy Cunningham <geromycunningham@gmail.com>
2017-12-02 19:25:27 +0000

    changed aesthetics

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $.ajax({
         method: "GET",
         url: "https://newsapi.org/v2/sources",
-        data: { category: "business", country: "us", language: "en", apiKey: APIKEY},
+        data: { country: "us", language: "en", apiKey: APIKEY},
         success: function(data) {
             if (data.status === "ok") {
                 console.log(data)
@@ -13,34 +13,40 @@ $(document).ready(function() {
                     source.innerHTML = data.sources[i].name;
                     document.getElementById('selection').appendChild(source);
                 }
-            } 
+            }
+            document.getElementById("selection").value = "";
         }
     })
     //     .done(function( data ) {
     //         console.log( data );
     //         console.log( data.status );
     //     });
-    $('#source').submit(function(event) {
-        event.preventDefault();
+    document.getElementById("selection").onchange = function() {
         document.getElementById("display").innerHTML = "";  
         $.ajax({
             method: "GET",
             url: "https://newsapi.org/v2/top-headlines",
-            data: { sources: document.getElementById("selection").value, category: "business", country: "us", language: "en", apiKey: APIKEY},
+            data: { sources: document.getElementById("selection").value, country: "us", language: "en", apiKey: APIKEY},
             success: function(event) {
                 if (event.status === "ok") {
                     console.log(event)
                     
                     for (var i = 0; i < event.articles.length; i++) {
                         var colDiv = document.createElement("DIV");
-                        colDiv.setAttribute("class", "col-xs-4");
-                        colDiv.setAttribute("style", "height: 450px");
-                        
-                        var headLink = document.createElement("H4");
+                        colDiv.setAttribute("class", "col-xs-10");
+                        colDiv.setAttribute("style", "height: 200px");
+
                         var picLink = document.createElement("A");
                         picLink.setAttribute("href", event.articles[i].url);
-                        picLink.innerHTML = "<img src="+event.articles[i].urlToImage+" height= 150vh> </br> "+event.articles[i].title+" </br>";
-                        headLink.appendChild(picLink);
+                        picLink.setAttribute("style", "float:left");
+                        picLink.innerHTML = "<img src="+event.articles[i].urlToImage+" height= 150vh>";
+                        colDiv.appendChild(picLink);
+                        
+                        var headLink = document.createElement("H4");
+                        var titleLink = document.createElement("A");
+                        titleLink.setAttribute("href", event.articles[i].url);
+                        titleLink.innerHTML = event.articles[i].title;
+                        headLink.appendChild(titleLink);
                         colDiv.appendChild(headLink);
 
                         var descrip = document.createElement("P");
@@ -52,5 +58,5 @@ $(document).ready(function() {
                 }
             }
         })
-    })
+    }
 })
